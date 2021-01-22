e1ac732e3f6a73517956133d1531a7a9d3b8c600
Garrett Walter <me@garrettwalter.dev>
2021-01-21 19:41:30 -0800

    Use package.json for jest, configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,41 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    browser: true,
+    node: true,
+    jest: true,
   },
-  'extends': [
+  extends: [
     'plugin:vue/essential',
     'eslint:recommended',
-    '@vue/typescript/recommended'
+    '@vue/airbnb',
+    '@vue/typescript/recommended',
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/no-unused-components': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'max-classes-per-file': 'off',
+    'no-plusplus': 'off',
+    // Article on no-restricted-syntax with "for...of" loops
+    // https://medium.com/@paul.beynon/thanks-for-taking-the-time-to-write-the-article-i-enjoyed-it-db916026647
+    'no-restricted-syntax': 'off',
+    'no-bitwise': 'warn',
+    'max-len': 'warn',
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/no-useless-constructor': 'error',
+    'no-continue': 'off', // https://github.com/airbnb/javascript/issues/1103
+    'no-underscore-dangle': ['error', { allowAfterThis: true }],
   },
   overrides: [
     {
       files: [
         '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
-      env: {
-        jest: true
-      }
-    }
-  ]
-}
+    },
+  ],
+};
